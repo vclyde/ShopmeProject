81983f046ac45f1a43885a5dc6716b1d5ee08f2f
vclyde <clydevelasquez@outlook.com>
2024-12-13 02:46:07 +0800

    Implement search/filter for users

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -30,25 +31,26 @@ public class UserController {
 //		List<User> listUsers = service.listAllUsers();
 //		model.addAttribute("listUsers", listUsers);
 //		return "users";
-		
-		return listByPage(1, model, "id", "asc");
+
+		return listByPage(1, model, "id", "asc", null);
 	}
-	
+
 	@GetMapping("/users/page/{pageNum}")
-	public String listByPage(@PathVariable int pageNum, Model model, 
-			@RequestParam String sortField, @RequestParam String sortDir) {
-		
-		Page<User> page = service.listByPage(pageNum, sortField, sortDir);
+	public String listByPage(@PathVariable int pageNum, Model model, @RequestParam String sortField,
+			@RequestParam String sortDir, @RequestParam(required = false) String keyword) {
+
+		Page<User> page = service.listByPage(pageNum, sortField, sortDir, keyword);
 		List<User> listUsers = page.getContent();
 		
+
 		long startCount = (pageNum - 1) * UserService.USERS_PER_PAGE + 1;
 		long endCount = startCount + UserService.USERS_PER_PAGE - 1;
 		if (endCount > page.getTotalElements()) {
 			endCount = page.getTotalElements();
 		}
-		
+
 		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
-		
+
 		model.addAttribute("currentPage", pageNum);
 		model.addAttribute("totalPages", page.getTotalPages());
 		model.addAttribute("startCount", startCount);
@@ -58,7 +60,10 @@ public class UserController {
 		model.addAttribute("sortField", sortField);
 		model.addAttribute("sortDir", sortDir); // Sort direction
 		model.addAttribute("reverseSortDir", reverseSortDir); // Reverse sort direction
+		model.addAttribute("keyword", keyword);
 		
+		System.out.println(keyword);
+
 		return "users";
 	}
 
@@ -91,7 +96,7 @@ public class UserController {
 			FileUploadUtil.cleanDir(uploadDir); // Delete other uploaded images before saving new image
 			FileUploadUtil.saveFile(uploadDir, filename, multipartFile);
 		} else {
-			
+
 			if (user.getPhotos().isEmpty()) {
 				user.setPhotos(null);
 			}
@@ -127,7 +132,7 @@ public class UserController {
 		try {
 			String uploadDir = "user-photos/" + id;
 			FileUploadUtil.cleanDir(uploadDir);
-			
+
 			service.delete(id);
 			redirectAttrib.addFlashAttribute("message", "The user ID " + id + " has been deleted successfully!");
 		} catch (UserNotFoundException e) {
