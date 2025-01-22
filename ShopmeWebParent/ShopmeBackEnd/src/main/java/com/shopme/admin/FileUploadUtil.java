a771cba3f8bf09a130e979e0c8fdc00d8b2ad79b
vclyde <clydevelasquez@outlook.com>
2025-01-22 14:06:08 +0800

    Configure logging for Admin

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/FileUploadUtil.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/FileUploadUtil.java
@@ -7,9 +7,14 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
 public class FileUploadUtil {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(FileUploadUtil.class);
+	
 
 	public static void saveFile(String uploadDir, String filename, MultipartFile multiPartFile) throws IOException {
 		
@@ -36,12 +41,14 @@ public class FileUploadUtil {
 					try {
 						Files.delete(file);
 					} catch(IOException ex) {
-						System.out.println("Could not delete file: " + file);
+						LOGGER.error("Could not delete file: " + file);
+//						System.out.println("Could not delete file: " + file);
 					}
 				}
 			});
 		} catch (IOException ex2) {
-			System.out.println("Could not list directory: " + dir);
+			LOGGER.error("Could not list directory: " + dir);
+//			System.out.println("Could not list directory: " + dir);
 		}
 	}
 }
